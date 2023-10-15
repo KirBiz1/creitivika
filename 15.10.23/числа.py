a23591f999629e5fd4337b3de7685bde625bf188
KirBiz1 <118673975+KirBiz1@users.noreply.github.com>
2023-10-15 14:32:48 +0300

    15.10.23
    Теперь и числа

---
+++ 15.10.23/числа.py
@@ -0,0 +1,66 @@
+"""
+A = input("Введите любое число")
+B = input("Введите любое число")
+C = input("Введите любое число")
+A = int(A)
+B = int(B)
+C = int(C)
+print("Сумма:", A + B + C)
+"""
+"""
+A = input("Введите любое число")
+B = input("Введите любое число")
+A = int(A)
+B = int(B)
+print("Cреднее арифметическое:",(A + B) / 2)
+"""
+"""
+friends1 = input("Введите любое число")
+friends2 = input("Введите любое число")
+friends3 = input("Введите любое число")
+friends1 = int(friends1)
+friends2 = int(friends2)
+friends3 = int(friends3)
+print("Всего:", friends1 + friends2 + friends3)
+print("Не хватает:", 15000 - (friends1 + friends2 + friends3))
+"""
+"""
+D = "100"
+E = "32"
+F = "8"
+A = input("Cколько купили хлеба?")
+B = input("Сколько купили молока?")
+C = input("Сколько купили сосисок?")
+A = int(A)
+B = int(B)
+C = int(C)
+D = int(D)
+E = int(E)
+F = int(F)
+print("Потратил:",(A * D) + (B * E) + (C * F))
+"""
+"""
+Vsego = input("Сколько денег у Васи в запасе?")
+Otdal1 = input("Сколько должен другу?")
+Otdal2 = input("Сколько должен тренеру?")
+Vsego = int(Vsego)
+Otdal1 = int(Otdal1)
+Otdal2 = int(Otdal2)
+print("У Васи осталось:",Vsego - (Otdal1 + Otdal2))
+"""
+"""
+Drug1 = "10000"
+Drug2 = "10000"
+Drug3 = "10000"
+Drug4 = "10000"
+Arenda  = "15500"
+Drug1 = int(Drug1)
+Drug2 = int(Drug2)
+Drug3 = int(Drug3)
+Drug4 = int(Drug4)
+Arenda = int(Arenda)
+print("На еду пойдет:", (Drug1 + Drug2 + Drug3 + Drug4) - Arenda)
+"""
+
+
+
