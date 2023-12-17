904238161cc1f9e5890c4bff111581a7335d850b
KirBiz1 <118673975+KirBiz1@users.noreply.github.com>
2023-12-17 18:27:43 +0700

    17.12.23 каскадные
    17.12

---
+++ 17.12.23.py
@@ -0,0 +1,15 @@
+"""
+login = input("Введите логин ")
+parol = input("Введите пароль ")
+if login != "abc123" and parol == "1234":
+    print("Неверный логин ")
+if login == "abc123" and parol != "1234":
+    print("Неверный пароль ")
+elif login == "abc123" and parol == "1234":
+    print("Все верно ")
+else:
+    print("Все неверно ")
+"""
+ 
+money = input("Сколько потрачено денег ")
+if money 
