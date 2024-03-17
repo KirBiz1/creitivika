bd7e271fb434598036ad6a8cd9b0f48e2652e18a
KirBiz1 <118673975+KirBiz1@users.noreply.github.com>
2024-03-17 18:25:43 +0700

    17.03 range()
    range

---
+++ 13/in range 17.03.py
@@ -0,0 +1,80 @@
+"""
+for i in range(50):
+    print(i)
+"""
+"""
+for i in range(50):
+    print(i+1)
+"""
+"""
+for i in range(1, 51, 2):
+    print(i)
+"""
+"""
+for i in range(60, 12, -6):
+    print(i)
+"""
+"""
+for i in range(60, 11, -6):
+    print(i)
+"""
+"""
+for i in range(60, 11, -6):
+    print(i, end="")
+"""
+"""
+1 задание
+for i in range(1,10):
+    print(i)
+"""
+
+"""
+2 задание
+for i in range(9,0,-1):
+    print(i)
+"""
+"""
+3 задание
+for i in range(2,23,4):
+    print(i)
+"""
+"""
+4 задание
+for i in range(1,47,9):
+    print(i)
+"""
+"""
+5 задание
+for i in range(50,29,-5):
+    print(i)
+"""
+"""
+6 задание
+hello = int(input("Сколько раз написать слово 'привет'"))
+for i in range(hello):
+    print("привет")
+"""
+"""
+7 задание
+num = 0
+for i in range(1,21):
+    num += 1
+print(num)
+"""
+"""
+for i in range(9):
+    text = input("Введите текст: ")
+    if text == "стоп":
+        break
+"""
+"""
+for i in range(1,10):
+    text = str(i) + ".txt"
+    print(text)
+"""
+ 
+import shutil 
+
+
+    
+
