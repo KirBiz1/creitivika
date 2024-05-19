d3b2ae782afc0ae429d6fffbb08ee0001165aebb
KirBiz1 <118673975+KirBiz1@users.noreply.github.com>
2024-05-19 16:29:05 +0700

    списки 19.05

---
+++ Списки..py
@@ -0,0 +1,79 @@
+"""
+slova = ["снежок", "пирожок", "СМЕТАААНА"]
+for slovo in slova:
+    print(slovo, end=" ") 
+"""
+"""
+slova = ["снежок", "пирожок", "СМЕТАААНА"]
+for slovo in slova:
+    print(slovo, end=" ") 
+
+slovo = input("Введите новое слово: ")
+slova.append(slovo)
+
+for slovo in slova:
+    print(slovo, end=" ") 
+"""
+"""
+slova = ["снежок", "пирожок", "СМЕТАААНА"]
+for slovo in slova:
+    print(slovo, end=" ") 
+
+slovo = input("Введите новое слово: ")
+slova.append(slovo)
+
+for slovo in slova:
+    print(slovo, end=" ") 
+"""
+"""
+slova = ["снежок","пирожок", "СМЕТАААНА"]
+slova[1] = "рогалик"
+print(slova) 
+"""
+"""
+frukt = ["яблоко","груша","дыня","виноград","манго"]
+print(frukt)
+frukt[0] = input("Введите новое 1 слово ")
+frukt[4] = input("Введите новое 6 слово ")
+print(frukt)
+"""
+"""
+play = ["Варфейс","Ворлд оф танкс","Вар робот"]
+print(play)
+slovo = input("Введите новое слово: ")
+play.append(slovo)
+
+slovo = input("Введите новое слово: ")
+play.append(slovo)
+
+for slovo in play:
+    print(slovo, end=" ")
+"""
+"""
+name = []
+
+slovo = input("Введите новое имя: ")
+name.append(slovo)
+
+slovo = input("Введите новое имя: ")
+name.append(slovo)
+
+slovo = input("Введите новое имя: ")
+name.append(slovo)
+
+slovo = input("Введите новое имя: ")
+name.append(slovo)
+
+slovo = input("Введите новое имя: ")
+name.append(slovo)
+
+slovo = input("Введите новое имя: ")
+name.append(slovo)
+print(name)
+"""
+
+animals = ["Кот","Лев","Тигр"]
+с = int(input("Какое слово заменить? "))
+if c == 1:
+    a = input("Введите новое слово ")
+    
